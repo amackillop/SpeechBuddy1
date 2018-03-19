7cf72a644ca4387404d7a76e99b1db97c66aa568
Moiz Muhammad <ezmoizy11@gmail.com>
2018-03-19 00:10:23 -0400

    testing python call for wpm

--- static/js/googleResponse.js
+++ static/js/googleResponse.js
@@ -1,16 +1,14 @@
 function googleResponse(data) {
-	console.log("google ran")
+	console.log("Google ran")
     transcript = document.getElementById("transcript");
     transcript.style.display = "block";
     content = document.getElementById("string");
-    //content.innerHTML = "\"" + (data.transcript).substr(2,data.transcript.length-3) + "\"";
     content.innerHTML=data.transcript;
 
     
     console.log("hello")
 
     confidence = document.getElementById("confidence");
-
     confidence.innerHTML = "Confidence.    " + data.confidence;
     confidence.style.display = "block";
     div = document.getElementById("recordingDiv");
