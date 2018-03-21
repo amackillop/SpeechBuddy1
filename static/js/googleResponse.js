0945c8ad77971ef87d9b9e2f5b395536d71e80b6
Moiz Muhammad <ezmoizy11@gmail.com>
2018-03-21 01:57:58 -0400

    Updated more UI.

--- static/js/googleResponse.js
+++ static/js/googleResponse.js
@@ -1,11 +1,12 @@
 function googleResponse(data) {
 	console.log("Google ran")
-    transcript = document.getElementById("transcript");
-    transcript.style.display = "block";
-    content = document.getElementById("string");
-    content.innerHTML=data.transcript;
+    text_title = document.getElementById("header-text");
+    text_title.innerHTML = "Your transcript: ";
+
+    transcript = document.getElementById("transcript-display");
+    transcript.innerHTML = data.transcript;
 
     confidence = document.getElementById("confidence");
-    confidence.innerHTML = "Confidence.    " + data.confidence;
+    confidence.innerHTML = "Confidence: " + data.confidence;
     confidence.style.display = "block";
 }
