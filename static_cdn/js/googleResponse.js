59b956a1b2361ad2c16b8e06b466d5c389180a15
Moiz Muhammad <ezmoizy11@gmail.com>
2018-04-01 02:50:08 -0400

    Updated analytics front end

--- static_cdn/js/googleResponse.js
+++ static_cdn/js/googleResponse.js
@@ -1,173 +1,158 @@
+
 function googleResponse(data) {
-    console.log("Google ran");
 
     //handle sliding + loading icon
     $("#myloader").hide();
-    $("#text-wrapper").show();
-    $("#right-split").animate({width:'100%'}, 1500);
-    $("#string").animate({width:'45%'}, 1000);
-   
-    $("#title").text("Here's what you said:");
-    $("#title").fadeIn( "fast" );
+    loadAnalyticsPage();
+    $("#right-split").animate({ width: '100%' }, 1500);
+    displayTranscriptWPM(data);
 
-    $("#transcript-display").text(data.transcript);
-    $("#transcript-display").fadeIn( "slow" );
-   
-    $('#right-split').prepend('<div class = "split right" id = "data-view"></div>');
-    var vol_n_pitch = `
-         <div id="myModal" class="modal">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <span class="close">&times;</span>
-                    <h2 id="mhead">Modal Header</h2>
-                </div>
-                <div class="modal-body" id="mbody"></div>
-                <div class="modal-footer">
-                    <h3>nltk lib</h3>
-                </div>
-            </div>
-        </div>
-        <div id = "data-wrapper">
-            <div id="fundementals">
-                <h2></h2>
-                <div id="chart_div" /div>
-                </div>
-                <div id="volume">
-                    <h2></h2>
-                    <div id="chart_div_volume" /div>
-                    </div>
-        
-                <div id="filler count">
-                    <div id="count">
-        
-                    </div>
-                </div>
-            </div>
-        </div>
-    `;
+    // $("#transcript-display").text(data.transcript);
+    // $("#transcript-display").fadeIn( "slow" );
 
-    $("#data-view").append(vol_n_pitch);
+    // $("#data-view").append(vol_n_pitch);
 
-    confidence = document.getElementById("confidence");
-    confidence.innerHTML = "Confidence: " + data.confidence;
-    confidence.style.display = "block";
+    // confidence = document.getElementById("confidence");
+    // confidence.innerHTML = "Confidence: " + data.confidence;
+    // confidence.style.display = "block";
 
-    
-    list_of_sentences = document.getElementById("list_of_sentences");
-    //list_of_sentences.innerHTML = data.list_of_sentences[0];
-
-    wordsperminute = document.getElementById("wordsperminute")
-    //wordsperminute.innerHTML = data.wordsperminute[0]
-    //console.log(data.wordsperminute.length);
-    console.log(data.list_of_sentences.length);
-
-    var temp1 = document.getElementById("Empty Transcript");
-    for(i = 0; i < data.wordsperminute.length+1 ; i++){
-        if(data.wordsperminute[i]<=120){
-            var temp3 = document.createElement("div"); 
-            temp3.setAttribute("class","too_slow");
-            // temp3.setAttribute("id","sen" + i);
-            var node = document.createTextNode(data.list_of_sentences[i]);
-            temp3.appendChild(node);
-            // $(temp3).append(node);
-            // $(temp1).append(temp3);
-            temp1.appendChild(temp3);
-        }
-         else if(data.wordsperminute[i] > 120 && data.wordsperminute[i]<= 140){
-            var temp3 = document.createElement("div"); 
-            temp3.setAttribute("class","slow");
-            // temp3.setAttribute("id","sen" + i);
-            var node = document.createTextNode(data.list_of_sentences[i]);
-            temp3.appendChild(node);
-            // $(temp3).append(node);
-            // $(temp1).append(temp3);
-            temp1.appendChild(temp3);
-
-         }
-        else if(data.wordsperminute[i] > 140 && data.wordsperminute[i] <= 170){
-            var temp3 = document.createElement("div"); 
-            temp3.setAttribute("class","good");
-            // temp3.setAttribute("id","sen" + i);
-            var node = document.createTextNode(data.list_of_sentences[i]);
-            temp3.appendChild(node);
-            // $(temp3).append(node);
-            // $(temp1).append(temp3);
-            temp1.appendChild(temp3);
- }
-         else if(data.wordsperminute[i] > 170 && data.wordsperminute[i] <= 190){
-            var temp3 = document.createElement("div"); 
-            temp3.setAttribute("class","fast");
-            // temp3.setAttribute("id","sen" + i);
-            var node = document.createTextNode(data.list_of_sentences[i]);
-            temp3.appendChild(node);
-            // $(temp3).append(node);
-            // $(temp1).append(temp3);
-            temp1.appendChild(temp3);
-        }
-         else if(data.wordsperminute[i] > 190){
-            var temp3 = document.createElement("div"); 
-            temp3.setAttribute("class","too_fast");
-            // temp3.setAttribute("id","sen" + i);
-            var node = document.createTextNode(data.list_of_sentences[i]);
-            temp3.appendChild(node);
-            // $(temp3).append(node);
-            // $(temp1).append(temp3);
-            temp1.appendChild(temp3);
-        }
-     }
 }
 
-function loadingTranscript(){
-    
+function loadingTranscript() {
+
     $("#submit-btn").prop('disabled', true);
     $("#header-text").remove();
     $("#text-wrapper").hide();
 
     $('#right-split').prepend('<div class = "loader" id = "myloader" align="center" hidden = "true"></div>');
-    $("#myloader").fadeIn( "slow" );
-
-    
-    //$("#transcript-display").hide();
+    $("#myloader").fadeIn("slow");
 }
 
-// function SentenceSpeed(data){
-//     //console.log("Testingtesting");
-//     //console.log(string(data.wordsperminute[0]));
-//     for(i = 0; i < data.wordsperminute.length ; i++){
-//         console.log(data.wordsperminute.length);
-//         if(data.wordsperminute[i]<=120){
-//             var temp=data.list_of_sentences[i];
-//             var temp2=document.getElementById("Empty Transcript").textContent;
-//             document.getElementById("Empty Transcript").textContent=temp2 + temp
-//             console.log("Temp is nOOOOOt");
-//         }
-//          else if(data.wordsperminute[i] > 120 && data.wordsperminute[i]<= 140){
-//             var temp=data.list_of_sentences[i];
-//             var temp2=document.getElementById("Empty Transcript").textContent;
-//             document.getElementById("Empty Transcript").textContent=temp2 + temp
-//             console.log("Temp is nOOOOOt2");
 
-//          }
-//         else if(data.wordsperminute[i] > 140 && data.wordsperminute[i] <= 170){
-//             var temp=data.list_of_sentences[i];
-//             var temp2=document.getElementById("Empty Transcript").textContent;
-//             document.getElementById("Empty Transcript").textContent=temp2 + temp
-//             console.log("Temp is nOOOOOt3");
+//this is a variable that contains the entire analytics page. It will be loaded once the google response returns
+var analytics_page = `
+    <div class="container-fluid">
+        <div class="row">  
+            <br>
 
-//  }
-//          else if(data.wordsperminute[i] > 170 && data.wordsperminute[i] <= 190){
-//             var temp=data.list_of_sentences[i];
-//             var temp2=document.getElementById("Empty Transcript").textContent;
-//             document.getElementById("Empty Transcript").textContent=temp2 + temp
-//             console.log("Temp is nOOOOOt4");
+            <div class="col-sm-12">
+                <div class="well well-sm " >
+                    <div style="text-align:left">
+                        <div>
+                            <p style="font-size:16px; margin:0px" class="btn" >Record / <b>Transcript Analysis</b></p>
+                        </div>
+                    </div>                      
+                </div>
+                <div class="row" style="height: 35vw">
+                    <div class="col-sm-6" >
+                        <div class="well" style="height: 34vw;overflow-y: scroll">
+                            <h2>Here's what you said:</h2>
+                            <h4 id ="empty-transcript" style="line-height: 2.4; margin: 40px">...</h4>
+                        </div>
+                    </div>   
 
-//         }
-//          else if(data.wordsperminute[i] > 190){
-//             var temp=data.list_of_sentences[i];
-//             var temp2=document.getElementById("Empty Transcript").textContent;
-//             document.getElementById("Empty Transcript").textContent=temp2 + temp
-//             console.log("Temp is nOOOOOt5");
+                    <div class="col-sm-1">
+                        <div class="well well-sm">
+                            <h4 style="font-size: 1vw"><b>Score</b></h4>
+                            <h4 style="font-size: 1.5vw" class = "med">82.5%</h4>
+                        </div>
+                    </div>
+                    <div class="col-sm-1">
+                        <div class="well well-sm">
+                            <h4 style="font-size: 1vw"><b>WPM</b></h4>
+                            <h4 style="font-size: 1.5vw" class="med">145</h4>
+                        </div>
+                    </div>
+                    <div class="col-sm-1">
+                        <div class="well well-sm">
+                            <h4 style="font-size: 1vw"><b>Words</b></h4>
+                            <h4 style="font-size: 1.5vw"  class="low">323</h4>
+                        </div>
+                    </div>
+                    <div class="col-sm-1">
+                        <div class="well well-sm">
+                            <h4 style="font-size: 1vw"><b>Fillers</b></h4>
+                            <h4 style="font-size: 1.5vw" class="very-high">7</h4>
+                        </div>
+                    </div>
+                    <div class="col-sm-1">
+                        <div class="well well-sm">
+                            <h4 style="font-size: 1vw"><b>Tone:</b></h4>
+                            <h4 style="font-size: 1.5vw" class="low">Neutral</h4>
+                        </div>
+                    </div>
+                    <div class="col-sm-1">
+                        <div class="well well-sm">
+                            <h4 style="font-size: 1vw"><b>Duration</b></h4>
+                            <h4 style="font-size: 1.5vw">2:49s</h4>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="well">
+                            <ul class="nav nav-tabs">
+                                <li class="active"><a href="#">Tone</a></li>
+                                <li ><a href="#">Volume</a></li>
+                                <li><a href="#">Corpus</a></li>
+                                <li><a href="#">Emotions</a></li>
+                            </ul>
+                            <div>
+                                <img style="height: 20vw;width:45vw" src="https://www.mathworks.com/matlabcentral/mlc-downloads/downloads/submissions/22214/versions/1/screenshot.jpg">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="well well-sm" style="text-align:left">
+                    <audio controls style="width: 95%; margin-top: 0px" src="audio/Simon_Sinek_30.flac"
+                        ontimeupdate="document.getElementById('tracktime').innerHTML = 'Track time: ' + Math.floor(this.currentTime) + ' / ' + Math.floor(this.duration);">
+                    </audio>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
 
-//         }
+function loadAnalyticsPage() {
+    $("#right-split").append(analytics_page);
+}
 
-//      }}
+function displayTranscriptWPM(data) {
+
+    var temp1 = document.getElementById("empty-transcript");
+    for (i = 0; i < data.wordsperminute.length - 1; i++) {
+        if (data.wordsperminute[i] <= 120) {
+            var temp3 = document.createElement("div");
+            temp3.setAttribute("class", "too_slow");
+            var node = document.createTextNode(data.list_of_sentences[i]);
+            temp3.appendChild(node);
+            temp1.appendChild(temp3);
+        }
+        else if (data.wordsperminute[i] > 120 && data.wordsperminute[i] <= 140) {
+            var temp3 = document.createElement("div");
+            temp3.setAttribute("class", "slow");
+            var node = document.createTextNode(data.list_of_sentences[i]);
+            temp3.appendChild(node);
+            temp1.appendChild(temp3);
+        }
+        else if (data.wordsperminute[i] > 140 && data.wordsperminute[i] <= 170) {
+            var temp3 = document.createElement("div");
+            temp3.setAttribute("class", "good");
+            var node = document.createTextNode(data.list_of_sentences[i]);
+            temp3.appendChild(node);
+            temp1.appendChild(temp3);
+        }
+        else if (data.wordsperminute[i] > 170 && data.wordsperminute[i] <= 190) {
+            var temp3 = document.createElement("div");
+            temp3.setAttribute("class", "fast");
+            var node = document.createTextNode(data.list_of_sentences[i]);
+            temp3.appendChild(node);
+            temp1.appendChild(temp3);
+        }
+        else if (data.wordsperminute[i] > 190) {
+            var temp3 = document.createElement("div");
+            temp3.setAttribute("class", "too_fast");
+            var node = document.createTextNode(data.list_of_sentences[i]);
+            temp3.appendChild(node);
+            temp1.appendChild(temp3);
+        }
+    }
+}
